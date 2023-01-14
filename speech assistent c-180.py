4e3292513325affe160b921c80c58f1b4fff7b01
Shin-B <87677114+Shin-B@users.noreply.github.com>
2023-01-14 13:31:41 -0800

    Add files via upload

---
+++ speech assistent c-180.py
@@ -0,0 +1,19 @@
+from tkinter import * 
+import speech_recognition as sr
+root = Tk()
+root.geometry("500x500")
+
+def r_audio():
+    speech_recognisor = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio= speech_recognisor.listen(source)
+        voice_data=''
+        try:
+            voice_data= speech_recognisor.recognize_google(audio, language='en-in')
+        except sr.UnknownValueError:
+            print('please repeat i did not get that')
+        print(voice_data)
+
+
+r_audio()
+root.mainloop()
